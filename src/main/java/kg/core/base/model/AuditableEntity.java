73fd5aab796d4b4065efbef128cb56720df7b6a7
Askat <kaskat6572@gmail.com>
2026-06-29 17:16:25 +0600

    инициализация данных

--- src/main/java/kg/core/base/model/AuditableEntity.java
+++ src/main/java/kg/core/base/model/AuditableEntity.java
@@ -1,14 +1,16 @@
 package kg.core.base.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 
+import kg.core.user.model.User;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
 
 import java.time.LocalDateTime;
 
@@ -25,4 +27,14 @@ public abstract class AuditableEntity extends BaseEntity {
     @UpdateTimestamp
     @Column(name = "updated_at", nullable = false)
     LocalDateTime updatedAt;
+
+    @CreatedBy
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "created_by")
+    private User createdBy;
+
+    @LastModifiedBy
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "updated_by")
+    private User updatedBy;
 }
